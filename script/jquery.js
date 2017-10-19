f61d8e57bf2b6975c5422851ebe81b6a9a8501c7
Jonathan Sebastian <jojosebseb@gmail.com>
2017-10-19 16:28:20 +0700

    v1

--- script/jquery.js
+++ script/jquery.js
@@ -10,3 +10,35 @@ $('.mobile-handle').on('click', function(){
     $('.nav-ul').toggleClass('active');
     $(this).toggleClass('active');
 })
+
+$('.product-slider').slick({
+    arrows: false,
+    infinite: false,
+    variableWidth: true,
+    focusOnSelect: true,
+    // centerMode: true
+})
+
+$('.product-gallery-container').slick({
+    slidesToShow: 4,
+    centerMode: true,
+})
+
+var curUrl;
+$('.product-gallery-image').on('click', function(){
+    curUrl = $(this).find('img').attr('src');
+    // console.log(curUrl);
+    $('.product-big-image').find('img').attr('src', curUrl);
+})
+
+$('#indexSlide  .absolute-center').addClass('active');
+
+console.log('asd');
+
+$('.product-container .product-module').each(function(i){
+    console.log(i);
+    $(this).addClass('active');
+    $(this).css({
+        transition: 'all 0.5s ease '+i/5+'s'
+    });
+})
